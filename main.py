f7bb1876401ddd039e086dc7ac477d927d45e3c6
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-02 17:11:50 -0500

    Añadir checks para cumplir con reglas de negocio

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 from typing import List
 
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from DataModel import DataModel, DataModelComplete
 from PredictionModel import PredictionModel
@@ -28,11 +28,49 @@ def read_root():
 
 @app.post("/predict")
 def make_prediction(dataModel: List[DataModel]):
-    df = pd.DataFrame([x.dict() for x in dataModel])
+    lista = []
+    for x in dataModel:
+        datos = x.dict()
+        check_gre(datos['gre_score'])
+        check_toefl(datos['toefl_score'])
+        check_university_rating(datos['university_rating'])
+        check_sop(datos['sop'])
+        check_lor(datos['lor'])
+        check_cpga(datos['cpga'])
+        check_research(datos['research'])
+        lista.append(datos)
+    df = pd.DataFrame(lista)
     df.columns = dataModel[0].columns()
     results = prediction_model.make_prediction(df)
     return results.tolist()
+    
+def check_gre(score:float):
+    if score > 340 or score < 0:
+        raise HTTPException(status_code=400, detail = "GRE debe ser menor o igual a 340 y mayor o igual a 0")
 
+def check_toefl(score:float):
+    if score > 120 or score < 0:
+        raise HTTPException(status_code=400, detail = "TOEFL debe ser menor o igual a 120 y mayor o igual a 0")
+
+def check_university_rating(score:float):
+    if score > 5 or score < 0:
+        raise HTTPException(status_code=400, detail = "University Rating debe ser menor o igual a 5 y mayor o igual a 0")
+
+def check_sop(score:float):
+    if score > 5 or score < 0:
+        raise HTTPException(status_code=400, detail = "SOP debe ser menor o igual a 5 y mayor o igual a 0")
+
+def check_lor(score:float):
+    if score > 5 or score < 0:
+        raise HTTPException(status_code=400, detail = "LOR debe ser menor o igual a 5 y mayor o igual a 0")
+
+def check_cpga(score:float):
+    if score > 10 or score < 0:
+        raise HTTPException(status_code=400, detail = "CGPA debe ser menor o igual a 10 y mayor o igual a 0")
+
+def check_research(score:float):
+    if score > 1 or score < 0:
+        raise HTTPException(status_code=400, detail = "Research debe ser 1 o 0")
 
 @app.post("/fit")
 def fit(dataModelComplete: List[DataModelComplete]):
